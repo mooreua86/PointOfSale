ed5561b33d3737736ef22dec1be31f2d291487d4
LAPTOP-42S1TSQU\Jacob <jmoore62@mail.csuchico.edu>
2016-10-31 16:23:56 -0700

    Took out debuging.

--- sales.js
+++ sales.js
@@ -4,7 +4,7 @@ function addItem()
 {
   var newItem;
   newItem = document.getElementById("price").value;
-  console.log(newItem);
+
   if (isNaN(newItem)) {
     alert("Enter price as a number");
   }
